b16112c1531b6241e2fb7913416ca01868a05cc7
Bryan Banda <bryanalexander.contact@gmail.com>
2026-01-06 20:09:15 -0300

    arreglo 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import WhatsAppWidget from "./components/WhatsappWidget";
+import WhatsAppWidget from "./components/WhatsAppWidget";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Portfolio from "./pages/Portfolio/Portfolio";
